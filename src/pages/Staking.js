3a9f237fed46f9bfdfa0f8bdc0e48448d7187cdb
mcbunnylau <mcbunnylau2013@gmail.com>
2022-09-12 10:01:51 +0100

    feat: merge staking logic from main to this branch

--- src/pages/Staking.js
+++ src/pages/Staking.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { useState } from "react";
 import Button from "../components/common/Button";
 import { Card } from "../components/common/Card";
 import { Container } from "../components/common/Container";
@@ -10,12 +11,273 @@ import {
   GridCell4,
   GridRow,
 } from "../components/common/Grid";
-import { Input, InputNumber } from "../components/common/Input";
+import { InputNumber } from "../components/common/Input";
 import Navbar from "../components/Navbar/Navbar";
 import StakeDiagram from "../components/Staking/StakeDiagram";
-import styles from "./Staking.module.css";
+import contractAddresses from "../static/contractAddresses";
+import BetterABI from "../static/ABI/BetterABI.json";
+import IERC20MetadataABI from "../static/ABI/IERC20MetadataABI.json";
 
-function Staking() {
+import styles from "./Staking.module.css";
+import {
+  useBalance,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+
+function Staking({ activeChain, connectedAddress }) {
+  // ---consts--------------------------
+
+  const BN = ethers.BigNumber.from;
+  const stakingContractConfig = {
+    addressOrName: contractAddresses[activeChain?.network]?.better,
+    contractInterface: BetterABI,
+  };
+
+  // ---reads----------------------------
+
+  // ------consts------------------------
+
+  // staking token addr
+  const { data: stakingTokenAddress } = useContractRead({
+    ...stakingContractConfig,
+    functionName: "getStakingToken",
+  });
+
+  // staking token decimals
+  const { data: stakingTokenDecimals } = useContractRead({
+    addressOrName: stakingTokenAddress,
+    contractInterface: IERC20MetadataABI,
+    functionName: "decimals",
+  });
+
+  // ------vars--------------------------
+
+  const [toStake, setToStake] = useState("0");
+  const [toUnstake, setToUnstake] = useState("0");
+
+  // staking token balance
+  const {
+    data: stakingTokenBalance,
+    isSuccess: stakingTokenBalanceSuccess,
+    refetch: refetchStakingTokenBalance,
+  } = useContractRead({
+    addressOrName: stakingTokenAddress,
+    contractInterface: IERC20MetadataABI,
+    functionName: "balanceOf",
+    args: connectedAddress,
+    watch: true,
+  });
+
+  // staking token allowance
+  const {
+    data: stakingTokenAllowance,
+    isSuccess: stakingTokenAllowanceSuccess,
+  } = useContractRead({
+    addressOrName: stakingTokenAddress,
+    contractInterface: IERC20MetadataABI,
+    functionName: "allowance",
+    args: [connectedAddress, stakingContractConfig.addressOrName],
+    watch: true,
+  });
+
+  // reward token balance
+  const {
+    data: rewardTokenBalance,
+    isSuccess: rewardTokenBalanceSuccess,
+    refetch: refetchRewardTokenBalance,
+  } = useBalance({
+    addressOrName: connectedAddress,
+  });
+
+  // currently staked
+  const {
+    data: staked,
+    isSuccess: stakedBalanceSuccess,
+    refetch: refetchStaked,
+  } = useContractRead({
+    ...stakingContractConfig,
+    functionName: "getStaked",
+    overrides: {
+      from: connectedAddress,
+    },
+  });
+
+  // pending reward tokens
+  const { data: rewards, isSuccess: pendingRewardsSuccess } = useContractRead({
+    ...stakingContractConfig,
+    functionName: "getPendingRewards",
+  });
+
+  // ---writes---------------------------
+
+  // stake approve
+  const { config: prepareWriteConfigApprove } = usePrepareContractWrite({
+    addressOrName: stakingTokenAddress,
+    contractInterface: IERC20MetadataABI,
+    functionName: "approve",
+    args: [
+      //spender
+      stakingContractConfig.addressOrName,
+      //value
+      min(parseStakingInput(toStake), BN(stakingTokenBalance)),
+    ],
+  });
+
+  const {
+    data: approvalTx,
+    isLoading: isApproving,
+    write: executeStakeApprove,
+  } = useContractWrite(prepareWriteConfigApprove);
+
+  // stake send
+  const { config: prepareWriteConfigStake } = usePrepareContractWrite({
+    ...stakingContractConfig,
+    functionName: "stake",
+    args: [0],
+  });
+  const { isLoading: isStaking, writeAsync: executeStake } = useContractWrite(
+    prepareWriteConfigStake
+  );
+
+  useWaitForTransaction({
+    hash: approvalTx?.hash,
+    onSuccess(data) {
+      executeStake?.({
+        recklesslySetUnpreparedArgs: [
+          min(parseStakingInput(toStake), BN(stakingTokenBalance)),
+        ],
+      }).then(() => {
+        refetchStakingTokenBalance?.();
+        refetchStaked?.();
+      });
+      console.log("Staking...");
+    },
+  });
+
+  // unstake
+  const { config: prepareWriteConfigUnstake } = usePrepareContractWrite({
+    ...stakingContractConfig,
+    functionName: "unstake",
+    args: [0],
+  });
+  const { isLoading: isUnstaking, writeAsync: executeUnstake } =
+    useContractWrite(prepareWriteConfigUnstake);
+
+  // claim
+  const { config: prepareWriteConfigClaim } = usePrepareContractWrite({
+    ...stakingContractConfig,
+    functionName: "claim",
+    overrides: {
+      from: connectedAddress,
+    },
+  });
+  const { isLoading: isClaiming, writeAsync: executeClaim } = useContractWrite(
+    prepareWriteConfigClaim
+  );
+
+  // ---visibility-----------------------
+
+  function fetchOrShow(bool, result, dec) {
+    if (bool && dec && result) {
+      return ethers.utils.formatUnits(result, dec);
+    }
+    return "Fetching...";
+  }
+
+  function stakingDisabled() {
+    return !parseStakingInput(toStake).gt(0) || isApproving || isStaking;
+  }
+
+  const stakingbuttonText = useCallback(
+    () => (isApproving ? "Approving..." : isStaking ? "Staking..." : "Stake"),
+    [isStaking, isApproving]
+  );
+
+  // ---functionality--------------------
+
+  function min(a, b) {
+    return a.gt(b) ? b : a;
+  }
+
+  function triggerStake(e) {
+    e.preventDefault();
+    if (parseStakingInput(toStake)?.gt(0)) {
+      executeStakeApprove?.();
+      console.log("Approving...");
+    }
+  }
+
+  function triggerUnstake(e) {
+    e.preventDefault();
+    if (parseStakingInput(toUnstake)?.gt(0)) {
+      executeUnstake?.({
+        recklesslySetUnpreparedArgs: min(
+          parseStakingInput(toUnstake),
+          BN(staked)
+        ),
+        recklesslySetUnpreparedOverrides: {
+          from: connectedAddress,
+        },
+      }).then(() => {
+        refetchStaked?.();
+        refetchStakingTokenBalance?.();
+      });
+      console.log("Unstaking...");
+    }
+  }
+
+  function triggerClaim(e) {
+    e.preventDefault();
+    if (parseStakingInput(rewards)?.gt(0)) {
+      executeClaim?.({
+        recklesslySetUnpreparedOverrides: {
+          from: connectedAddress,
+        },
+      }).then(() => {
+        refetchRewardTokenBalance?.();
+      });
+      console.log("Claiming...");
+    }
+  }
+
+  function isNumeric(i) {
+    return !isNaN(parseFloat(i)) && isFinite(i);
+  }
+
+  function parseStakingInput(i) {
+    if (isNumeric(i) && stakingTokenDecimals) {
+      return ethers.utils.parseUnits(i, stakingTokenDecimals);
+    }
+    return BN(0);
+  }
+
+  const getStakingTokenBalance = useCallback(
+    () =>
+      fetchOrShow(
+        stakingTokenBalanceSuccess,
+        stakingTokenBalance,
+        stakingTokenDecimals
+      ),
+    [stakingTokenBalanceSuccess, stakingTokenBalance, stakingTokenDecimals]
+  );
+  const getStakingTokenAllowance = useCallback(
+    () =>
+      fetchOrShow(
+        stakingTokenAllowanceSuccess,
+        stakingTokenAllowance,
+        stakingTokenDecimals
+      ),
+    [stakingTokenAllowanceSuccess, stakingTokenAllowance, stakingTokenDecimals]
+  );
+  const getStaked = useCallback(
+    () => fetchOrShow(stakedBalanceSuccess, staked, stakingTokenDecimals),
+    [stakedBalanceSuccess, staked, stakingTokenDecimals]
+  );
+
+  // ui code
   const [bridgeAmount, setBridgeAmount] = useState(0);
   const [stakeLpAmount, setStakeLpAmount] = useState(0);
   const [stakeBtAmount, setStakeBtAmount] = useState(0);
