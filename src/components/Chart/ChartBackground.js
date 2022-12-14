de8d154129e9df84c63d3fcc08cc90b3b0324e3b
mcbunnylau <mcbunnylau2013@gmail.com>
2022-12-14 18:05:35 +0000

    feat: added horizontal guidelines for y axis and temp labels for easy comparison

--- src/components/Chart/ChartBackground.js
+++ src/components/Chart/ChartBackground.js
@@ -114,115 +114,120 @@ const ChartBackground = (props) => {
     return dataPointList;
   };
 
-  // const getHorizontalBackground = () => {
-  //   console.log("getHorizontalBackground chartConfig", props.chartConfig);
-  //   let data = transpose(props.data);
-  //   const { oldRangeInfo, newRangeInfo, middleCoords, yLabelSize } =
-  //     props.rangeInfo(
-  //       data,
-  //       props.chartConfig,
-  //       props.epochData,
-  //       props.instrument
-  //     );
-  //   console.log("getHorizontalBackground yLabelSize", yLabelSize);
-  //   console.log("getHorizontalBackground transpose", transpose);
-  //   console.log(
-  //     "getHorizontalBackground { oldRangeInfo, newRangeInfo, middleCoords }",
-  //     { oldRangeInfo, newRangeInfo, middleCoords }
-  //   );
+  const getHorizontalBackground = () => {
+    console.log("getHorizontalBackground chartConfig", props.chartConfig);
+    let data = transpose(props.data);
+    const {
+      oldRangeInfo,
+      newRangeInfo,
+      epochStartPoint: middleCoords,
+      yLabelSize,
+    } = props.rangeInfo(
+      data,
+      props.chartConfig,
+      props.epochData,
+      props.instrument
+    );
+    console.log("getHorizontalBackground yLabelSize", yLabelSize);
+    console.log("getHorizontalBackground transpose", transpose);
+    console.log(
+      "getHorizontalBackground { oldRangeInfo, newRangeInfo, middleCoords }",
+      { oldRangeInfo, newRangeInfo, middleCoords }
+    );
 
-  //   let dataPointList = [];
+    let dataPointList = [];
 
-  //   /* generate coord for y axis labels */
-  //   let yLabelCoordList = [];
-  //   let yAxisRange = oldRangeInfo[1][2];
-  //   console.log("getHorizontalBackground yAxisRange", yAxisRange);
+    /* generate coord for y axis labels */
+    let yLabelCoordList = [];
+    let yAxisRange = oldRangeInfo[1][2];
+    console.log("getHorizontalBackground yAxisRange", yAxisRange);
 
-  //   let interval = priceRange2PriceInterval(1);
-  //   console.log("getHorizontalBackground interval", interval);
-  //   let yMiddle = middleCoords[1];
-  //   console.log("getHorizontalBackground yMiddle", yMiddle);
+    let interval = priceRange2PriceInterval();
+    console.log("getHorizontalBackground interval", interval);
 
-  //   // lower prices
+    for (let i = 0; i < 8; i++) {
+      yLabelCoordList.push([0, oldRangeInfo[1][0] + yLabelSize * i]);
+    }
 
-  //   // higher prices
+    console.log("getHorizontalBackground yLabelCoordList", yLabelCoordList);
 
-  //   // get y labels for each coord
-  //   let yLabelList = transpose(yLabelCoordList)[1];
-  //   console.log("getHorizontalBackground yLabelList", yLabelList);
-  //   yLabelList = yLabelList.map((price) => price.toPrecision(5));
+    // get y labels for each coord
+    let yLabelList = transpose(yLabelCoordList)[1];
+    console.log("getHorizontalBackground yLabelList", yLabelList);
+    yLabelList = yLabelList.map((price) => price.toPrecision(5));
 
-  //   // scale coords to fit svg
-  //   yLabelCoordList = transpose(yLabelCoordList);
-  //   yLabelCoordList = data2SvgView(
-  //     yLabelCoordList,
-  //     oldRangeInfo,
-  //     newRangeInfo,
-  //     props.chartConfig.containerHeight
-  //   );
+    // scale coords to fit svg
+    yLabelCoordList = transpose(yLabelCoordList);
+    yLabelCoordList = data2SvgView(
+      yLabelCoordList,
+      oldRangeInfo,
+      newRangeInfo,
+      props.chartConfig.containerHeight
+    );
 
-  //   // move data to correct position
-  //   yLabelCoordList[0] = yLabelCoordList[0].map(() =>
-  //     props.chartConfig.paddingX()
-  //   );
+    // move data to correct position
+    yLabelCoordList[0] = yLabelCoordList[0].map(() =>
+      props.chartConfig.paddingX()
+    );
 
-  //   yLabelCoordList = transpose(yLabelCoordList);
+    yLabelCoordList = transpose(yLabelCoordList);
 
-  //   // add label element
-  //   yLabelCoordList.map((y, i) => {
-  //     console.log("getHorizontalBackground y", y);
-  //     // text string
-  //     dataPointList.push(
-  //       <text
-  //         x={y[0] - 5}
-  //         y={y[1]}
-  //         fill="red"
-  //         textAnchor="end"
-  //         alignmentBaseline="middle"
-  //       >
-  //         {yLabelList[i]}
-  //       </text>
-  //     );
+    // add label element
+    yLabelCoordList.map((y, i) => {
+      console.log("getHorizontalBackground y", y);
+      // text string
+      dataPointList.push(
+        <text
+          x={y[0] - 5}
+          y={y[1]}
+          fill="red"
+          textAnchor="end"
+          alignmentBaseline="middle"
+        >
+          {yLabelList[i]}
+        </text>
+      );
 
-  //     // vertical lines on chart
-  //     dataPointList.push(
-  //       <line
-  //         x1={props.chartConfig.paddingX()}
-  //         y1={y[1]}
-  //         x2={props.chartConfig.paddingX() + props.chartConfig.chartWidth}
-  //         y2={y[1]}
-  //         stroke="grey"
-  //         strokeDasharray="5"
-  //       />
-  //     );
-  //   });
+      // vertical lines on chart
+      dataPointList.push(
+        <line
+          x1={0}
+          y1={y[1]}
+          x2={props.chartConfig.containerWidth}
+          y2={y[1]}
+          stroke="grey"
+          strokeDasharray="5"
+        />
+      );
+    });
 
-  //   /* generate y coords */
+    /* generate y coords */
 
-  //   return dataPointList;
-  // };
+    return dataPointList;
+  };
 
   return (
     <>
       {props.data && props.epochData ? getVerticalBackground() : null}
-      {/* {props.data && props.epochData ? getHorizontalBackground() : null} */}
-      {/* x axis */}
-      <line
+      {props.data && props.epochData ? getHorizontalBackground() : null}
+
+      {/* y axis */}
+      {/* <line
         x1={props.chartConfig.paddingX() + 0}
         y1={props.chartConfig.paddingY() + 0}
         x2={props.chartConfig.paddingX() + 0}
         y2={props.chartConfig.paddingY() + props.chartConfig.chartHeight}
         stroke="grey"
-      />
+      /> */}
 
-      {/* y axis */}
-      <line
+      {/* x axis */}
+      {/* <line
         x1={props.chartConfig.paddingX() + 0}
         y1={props.chartConfig.paddingY() + props.chartConfig.chartHeight}
         x2={props.chartConfig.paddingX() + props.chartConfig.chartWidth}
         y2={props.chartConfig.paddingY() + props.chartConfig.chartHeight}
         stroke="grey"
-      />
+      /> */}
 
       {/* axis label */}
 
