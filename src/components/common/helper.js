946565fe8db081b121abfc7eea92b80cfcae8953
mcbunnylau <mcbunnylau2013@gmail.com>
2023-01-09 01:02:38 +0000

    feat: created helper function to generate instrument labels

--- src/components/common/helper.js
+++ src/components/common/helper.js
@@ -9,4 +9,17 @@ const trimNumber = (number = 0, count = 0, type = "dp") => {
   }
 };
 
-export { trimNumber };
+const instrumentLabel = (instrument) => {
+  if (instrument) {
+    return `${instrument.underlyingDescription.replaceAll(" ", "")} ${
+      (+instrument.epochDurationInSeconds +
+        +instrument.bufferDurationInSeconds) /
+      60
+    }m (${(+instrument.volatilityMultiplier / 10000).toFixed(1)} SD, ${
+      +instrument.baseError / 10000
+    } E)`;
+  }
+  return ``;
+};
+
+export { trimNumber, instrumentLabel };
