2578b5ee354ce5b5713b6c88fdff3516fba36f3e
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2023-03-13 13:57:39 -0700

    Update useDbmtPrice.js

--- src/hooks/useDbmtPrice.js
+++ src/hooks/useDbmtPrice.js
@@ -10,7 +10,7 @@ export default function useDbmtPrice() {
     address: contractAddresses[chain?.network]?.dbmtSale,
     abi: DBMTSaleABI  ,
     functionName: "BASE_PRICE_IN_WEI",
-    select: (data) => ethers.utils.formatEther(data),
+    select: (data) => Number(ethers.utils.formatEther(data)),
   });
 
   const { data: priceMulti } = useContractRead({
