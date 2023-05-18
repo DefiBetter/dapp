9a7c41dc4e70f89377a7caa85b7b70eadc010492
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2023-05-17 21:21:30 -0700

    adding UI

--- src/pages/Dbmt.js
+++ src/pages/Dbmt.js
@@ -12,7 +12,7 @@ import useDbmtBuy from "../hooks/useDbmtBuy";
 import DBButton from "../components/common/DBButton";
 import Loader from "../components/common/Loader";
 import useNativeBalance from "../hooks/useNativeBalance";
-
+// import { AiOutlineCopy } from "react-icons/ai";
 import { GiTwoCoins, GiWallet, GiPriceTag } from "react-icons/gi";
 import AddToWallet from "../components/common/AddToWallet";
 import PageTitle from "../components/common/PageTitle";
@@ -22,12 +22,20 @@ import useDmbtMinPayment from "../hooks/useDmbtMinPayment";
 import ContainerStats from "../components/common/ContainerStats.js";
 import useSymbol from "../hooks/useSymbol";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { useAccount } from "wagmi";
+import { AiFillCopy, AiOutlineInfoCircle } from "react-icons/ai";
 
 export default function Dbmt() {
+  const queryParameters = new URLSearchParams(window.location.search);
+  const referral = queryParameters.get("ref");
+
+  const { address } = useAccount();
   const [input, setInput] = useState();
   const [buyAmount, setBuyAmount] = useState("0");
   const [dbmtBuyAmount, setDbmtBuyAmount] = useState("0");
   const [toggleReadMore, setToggleReadMore] = useState(false);
+  const [showReferralInfo, setShowReferralInfo] = useState(false)
+  const [successCopy, setSuccessCopy] = useState(false);
   const dbmtPerEth = useDbmtPerEth(buyAmount);
   const ethPerDbmt = useEthPerDbmt(dbmtBuyAmount);
 
@@ -48,6 +56,8 @@ export default function Dbmt() {
   const { chain } = useNetwork();
   const minAmount = useDmbtMinPayment();
   const buyWrite = useDbmtBuy(buyAmount);
+  const claim = useDbmtBuy(buyAmount); //useDbmtClaimRewards();
+
   const userGasBalance = useNativeBalance();
   const tokenSymbol = useSymbol(contractAddresses[chain?.network]?.dbmtToken);
 
@@ -233,138 +243,225 @@ export default function Dbmt() {
               </div>
             </div>
           </div>
-          <div className="w-full lg:w-2/3 bg-white dark:bg-db-dark rounded-lg overflow-hidden ">
-            <div className="w-full bg-db-cyan-process pb-2">
-              <div className="flex flex-col md:flex-row justify-center items-center gap-0 md:gap-5 py-1">
-                {isSale && (
-                  <div className="text-2xl font-bold relative pt-2 text-black">
-                    1 {tokenSymbol} = {basePrice ? basePrice.toFixed(3) : 0}{" "}
-                    {nativeGasToken}
-                    <div className="absolute bottom-[35%] left-[0%] w-full h-1 bg-gradient-to-r from-red-400 to-orange-500"></div>
-                  </div>
-                )}
-                {isSale && (
-                  <div className="hidden md:block pt-2">
-                    <CgArrowLongRight size={40} className="text-white" />
-                  </div>
-                )}
-                <div className="font-bold text-3xl text-white">
-                  1 {tokenSymbol} = {currentPrice ? currentPrice.toFixed(3) : 0}{" "}
-                  {nativeGasToken}
-                </div>
-              </div>
-              {isSale && (
-                <div className="text-center text-xs italic text-white">
-                  Limited time offer only
-                </div>
-              )}
-            </div>
-            <div className="p-4">
-              <ContainerStats
-                stats={[
-                  {
-                    label: "Your Wallet Balance",
-                    icon: <GiWallet size={20} />,
-                    value1: `${userGasBalance}`,
-                    value2: `${nativeGasToken}`,
-                  },
-                  {
-                    label: "Supply left",
-                    icon: <GiTwoCoins size={20} />,
-                    value1: trimNumber(+supplyLeft, 4, "dp"),
-                  },
-                  {
-                    label: "Min. Purchase",
-                    icon: <GiPriceTag size={20} />,
-                    value1: `${minAmount.toFixed(2)} ${nativeGasToken}`,
-                  },
-                ]}
-              />
-
-              <div className="mt-4 flex justify-center w-full">
-                <div className="w-full gap-2 flex">
-                  <div className="w-16 md:w-20 flex justify-center items-center">
-                    <span className="text-xl">Buy</span>
-                  </div>
-                  <div className="h-14 w-full bg-white dark:bg-db-dark-input shadow-inner shadow-db-cyan-process dark:shadow-black rounded-lg flex gap-2 items-center px-4">
-                    <input
-                      value={Number(dbmtBuyAmount) !== 0 ? dbmtBuyAmount : ""}
-                      onChange={(e) => {
-                        setInput(0);
-                        let val = e.target.value || "0";
-                        if (Number(val) > supplyLeft) {
-                          val = supplyLeft.toString();
-                        }
-                        setDbmtBuyAmount(val);
-                        if (val === "0") {
-                          setBuyAmount("0");
-                        }
-                      }}
-                      type={"number"}
-                      min={0}
-                      max={Number(supplyLeft)}
-                      className="text-left md:text-center px-0 md:px-4 h-10 w-full focus:ring-0 focus:outline-none rounded-lg bg-white dark:bg-db-dark-input"
-                      placeholder={`${tokenSymbol} amount`}
-                    />
-                    <div className="">{tokenSymbol}</div>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-4 flex justify-center w-full">
-                <div className="w-full gap-2 flex">
-                  <div className="w-16 md:w-20 flex justify-center items-center">
-                    <span className="text-xl">For</span>
-                  </div>
-                  <div className="h-14 w-full bg-white dark:bg-db-dark-input rounded-lg flex gap-4 items-center px-4 shadow-inner shadow-db-cyan-process dark:shadow-black">
-                    <input
-                      value={Number(buyAmount) !== 0 ? buyAmount : ""}
-                      onChange={(e) => {
-                        setInput(1);
-                        const val = e.target.value || "0";
-                        setBuyAmount(val);
-                        if (val === "0") {
-                          setDbmtBuyAmount("0");
-                        }
-                      }}
-                      type={"number"}
-                      min={0}
-                      className="text-left md:text-center md:pl-20 px-0 md:px-4 h-10 w-full focus:ring-0 focus:outline-none rounded-lg bg-white dark:bg-db-dark-input"
-                      placeholder={`${nativeGasToken} amount`}
-                    />
-                    <div
-                      onClick={() => {
-                        setInput(1);
-                        setBuyAmount(
-                          (Number(userGasBalance) - 0.0001).toString()
-                        );
-                      }}
-                      className="cursor-pointer rounded-lg flex justify-center items-center h-9 pb-0.5 px-2  border-[1px] border-db-cyan-process text-db-cyan-process hover:bg-db-cyan-process hover:text-white transition-colors"
-                    >
-                      MAX
+          <div className="w-full lg:w-2/3 flex flex-col lg:flex-row gap-4">
+            <div className="w-full lg:w-2/3 bg-white dark:bg-db-dark rounded-lg overflow-hidden">
+              <div className="w-full bg-db-cyan-process pb-2">
+                <div className="flex flex-col md:flex-row justify-center items-center gap-0 md:gap-5 py-1">
+                  {isSale && (
+                    <div className="text-2xl font-bold relative pt-2 text-black">
+                      1 {tokenSymbol} = {basePrice ? basePrice.toFixed(3) : 0}{" "}
+                      {nativeGasToken}
+                      <div className="absolute bottom-[35%] left-[0%] w-full h-1 bg-gradient-to-r from-red-400 to-orange-500"></div>
                     </div>
-                    <div className="">{nativeGasToken}</div>
+                  )}
+                  {isSale && (
+                    <div className="hidden md:block pt-2">
+                      <CgArrowLongRight size={40} className="text-white" />
+                    </div>
+                  )}
+                  <div className="font-bold text-3xl text-white">
+                    1 {tokenSymbol} ={" "}
+                    {currentPrice ? currentPrice.toFixed(3) : 0}{" "}
+                    {nativeGasToken}
+                  </div>
+                </div>
+                {isSale && (
+                  <div className="text-center text-xs italic text-white">
+                    Limited time offer only
+                  </div>
+                )}
+              </div>
+              <div className="p-4">
+                <ContainerStats
+                  stats={[
+                    {
+                      label: "Your Wallet Balance",
+                      icon: <GiWallet size={20} />,
+                      value1: `${userGasBalance}`,
+                      value2: `${nativeGasToken}`,
+                    },
+                    {
+                      label: "Supply left",
+                      icon: <GiTwoCoins size={20} />,
+                      value1: trimNumber(+supplyLeft, 4, "dp"),
+                    },
+                    {
+                      label: "Min. Purchase",
+                      icon: <GiPriceTag size={20} />,
+                      value1: `${minAmount.toFixed(2)} ${nativeGasToken}`,
+                    },
+                  ]}
+                />
+
+                <div className="mt-4 flex justify-center w-full">
+                  <div className="w-full gap-2 flex">
+                    <div className="w-16 md:w-20 flex justify-center items-center">
+                      <span className="text-xl">Buy</span>
+                    </div>
+                    <div className="h-14 w-full bg-white dark:bg-db-dark-input shadow-inner shadow-db-cyan-process dark:shadow-black rounded-lg flex gap-2 items-center px-4">
+                      <input
+                        value={Number(dbmtBuyAmount) !== 0 ? dbmtBuyAmount : ""}
+                        onChange={(e) => {
+                          setInput(0);
+                          let val = e.target.value || "0";
+                          if (Number(val) > supplyLeft) {
+                            val = supplyLeft.toString();
+                          }
+                          setDbmtBuyAmount(val);
+                          if (val === "0") {
+                            setBuyAmount("0");
+                          }
+                        }}
+                        type={"number"}
+                        min={0}
+                        max={Number(supplyLeft)}
+                        className="text-left md:text-center px-0 md:px-4 h-10 w-full focus:ring-0 focus:outline-none rounded-lg bg-white dark:bg-db-dark-input"
+                        placeholder={`${tokenSymbol} amount`}
+                      />
+                      <div className="">{tokenSymbol}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-4 flex justify-center w-full">
+                  <div className="w-full gap-2 flex">
+                    <div className="w-16 md:w-20 flex justify-center items-center">
+                      <span className="text-xl">For</span>
+                    </div>
+                    <div className="h-14 w-full bg-white dark:bg-db-dark-input rounded-lg flex gap-4 items-center px-4 shadow-inner shadow-db-cyan-process dark:shadow-black">
+                      <input
+                        value={Number(buyAmount) !== 0 ? buyAmount : ""}
+                        onChange={(e) => {
+                          setInput(1);
+                          const val = e.target.value || "0";
+                          setBuyAmount(val);
+                          if (val === "0") {
+                            setDbmtBuyAmount("0");
+                          }
+                        }}
+                        type={"number"}
+                        min={0}
+                        className="text-left md:text-center md:pl-20 px-0 md:px-4 h-10 w-full focus:ring-0 focus:outline-none rounded-lg bg-white dark:bg-db-dark-input"
+                        placeholder={`${nativeGasToken} amount`}
+                      />
+                      <div
+                        onClick={() => {
+                          setInput(1);
+                          setBuyAmount(
+                            (Number(userGasBalance) - 0.0001).toString()
+                          );
+                        }}
+                        className="cursor-pointer rounded-lg flex justify-center items-center h-9 pb-0.5 px-2  border-[1px] border-db-cyan-process text-db-cyan-process hover:bg-db-cyan-process hover:text-white transition-colors"
+                      >
+                        MAX
+                      </div>
+                      <div className="">{nativeGasToken}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-4 flex gap-4 items-center flex-col md:flex-row justify-center">
+                  <div className="w-full md:w-2/3">
+                    <DBButton
+                      disabled={!buyWrite.transaction.write}
+                      onClick={() => {
+                        if (buyWrite.transaction.write) {
+                          buyWrite.transaction.write();
+                        }
+                      }}
+                    >
+                      {buyWrite.confirmation.isLoading ? (
+                        <Loader text="Buying" />
+                      ) : (
+                        "Buy"
+                      )}
+                    </DBButton>
+                  </div>
+                  <div className="w-full md:w-1/3">
+                    <AddToWallet asset="DBMT" />
                   </div>
                 </div>
               </div>
-              <div className="mt-4 flex gap-4 items-center flex-col md:flex-row justify-center">
-                <div className="w-full md:w-2/3">
+            </div>
+            <div className="w-full lg:w-1/3 bg-white dark:bg-db-dark rounded-lg overflow-hidden p-4">
+              <div className="flex flex-col w-full h-full justify-between">
+                <div className="relative text-center w-full text-xl flex gap-4 justify-center items-center">
+                  DBMT Referral
+                  <AiOutlineInfoCircle
+                    size={25}
+                    className="text-db-cyan-process"
+                    onMouseEnter={() => setShowReferralInfo(true)}
+                    onMouseLeave={() => setShowReferralInfo(false)}
+                  />
+                  {showReferralInfo && (
+                    <div className='absolute left-0 top-8 bg-db-light dark:bg-db-dark-info rounded-lg p-2 text-xs'>Recommend DeFiBetter and bring in new investors to earn incentives and BNB. Referees must invest a minimum of $50.</div>
+                  )}
+                </div>
+
+                <div className="mt-4 w-full flex justify-between items-center">
+                  <div className="text-db-cyan-process">Your $DBMT</div>
+                  <div>4.24</div>
+                </div>
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-db-cyan-process">Referral % rewards</div>
+                  <div>5%</div>
+                </div>
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-db-cyan-process">Raised</div>
+                  <div>412 BNB</div>
+                </div>
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-db-cyan-process">Profit from Raised</div>
+                  <div>24.21 BNB</div>
+                </div>
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-db-cyan-process">Pending Rewards</div>
+                  <div>4.21 BNB</div>
+                </div>
+                <div className="mt-4">
                   <DBButton
-                    disabled={!buyWrite.transaction.write}
+                    disabled={!claim.transaction.write}
+                    heigthTwClass={"h-10"}
                     onClick={() => {
-                      if (buyWrite.transaction.write) {
-                        buyWrite.transaction.write();
+                      if (claim.transaction.write) {
+                        claim.transaction.write();
                       }
                     }}
                   >
-                    {buyWrite.confirmation.isLoading ? (
-                      <Loader text="Buying" />
+                    {claim.confirmation.isLoading ? (
+                      <Loader text="Claiming Rewards" />
                     ) : (
-                      "Buy"
+                      "Claim Rewards"
                     )}
                   </DBButton>
                 </div>
-                <div className="w-full md:w-1/3">
-                  <AddToWallet asset="DBMT" />
+                <div
+                  className="mt-4 overflow-hidden cursor-pointer"
+                  onClick={() => {
+                    setSuccessCopy(true);
+                    navigator.clipboard.writeText(
+                      `${window.location.origin}${window.location.pathname}?ref=${address}`
+                    );
+                    setTimeout(() => {
+                      setSuccessCopy(false);
+                    }, 3000);
+                  }}
+                >
+                  <div className="text-db-cyan-process">
+                    Your Referral Link (click to copy)
+                  </div>
+                  <div className="w-full flex gap-4 items-center cursor-pointer justify-between">
+                    <div className="w-full text-db-dark-info overflow-hidden">
+                      .../ref={address}...
+                    </div>
+                    <AiFillCopy
+                      className="text-db-cyan-process active:text-db-dark"
+                      size={20}
+                    />
+                  </div>
+                </div>
+                <div className="h-5 text-green-500 text-xs text-center">
+                  {successCopy && (
+                    <span>Copied referral link to clipboard</span>
+                  )}
                 </div>
               </div>
             </div>
