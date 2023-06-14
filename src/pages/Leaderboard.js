12424db098ded9c107715c323ac90b6671155c68
Tazz <104685150+ItsMeTazz@users.noreply.github.com>
2023-06-13 19:11:41 -0700

    Update Leaderboard.js

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -29,6 +29,12 @@ export default function Leaderboard() {
       >
         <PageTitle title={tokenSymbol} fancyTitle={"Leaderboard"} />
 
+        REACT_APP_API_KEY = {process.env.REACT_APP_API_KEY}<br />
+        REACT_APP_AUTH_DOMAIN = {process.env.REACT_APP_AUTH_DOMAIN}<br />
+        REACT_APP_PROJECT = {process.env.REACT_APP_PROJECT}<br />
+        REACT_APP_STORAGE = {process.env.REACT_APP_STORAGE}<br />
+        REACT_APP_MESSAGING_ID = {process.env.REACT_APP_MESSAGING_ID}<br />
+        REACT_APP_APP_ID = {process.env.REACT_APP_APP_ID}<br />
         <div className="mt-2 md:mt-4 flex flex-col justify-center items-center gap-4">
           <div className="w-full lg:w-2/3">
             <div
